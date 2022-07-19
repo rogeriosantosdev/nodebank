a4bb6c34f5ab1ef2377cfd9e5bcfd258000b1343
Rogerio Santos <rogeriosantosdev@gmail.com>
2022-07-19 10:43:34 -0300

    Realizando criação e verificação de contas

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //Módulo externo
 import inquirer from 'inquirer'
+import chalk from 'chalk'
 
 //Módulo interno
 import fs from 'fs'
+import { clear } from 'console'
 
 operation()
 
@@ -21,7 +23,54 @@ function operation(){
     }]
     ).then((answer) => {
         const action = answer['action']
-        console.log(action)
+        
+        switch(action){
+            case 'Criar Conta':
+                createAccount()
+                break;
+        }
     })
     .catch((err) => {console.log(err)})
+}
+
+//create an account
+function createAccount(){
+    console.log("Obrigado por escolher o nosso banco!")
+    console.log("Defina as opções da sua conta a seguir.")
+
+    buildAccount()
+}
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite o nome para sua conta bancária: '
+        }
+    ]).then((answer) => {
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Conta existente no nosso banco de dados, por favor, utilize outro nome!'))
+            buildAccount()
+            return
+        } else if (accountName == ''){
+            console.log(chalk.bgRed.black("Informe um nome para a sua conta"))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', (err) => {
+            console.log(err)
+        })
+
+        clear()
+        console.log(chalk.bgGreen.black("Parabéns, sua conta foi registrada com sucesso!"))
+        operation()
+    }).catch((err) => {console.log(err)})
 }
