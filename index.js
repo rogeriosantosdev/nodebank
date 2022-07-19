d140dcf6847b9739b736eb614e05dc53dbc69ff6
Rogerio Santos <rogeriosantosdev@gmail.com>
2022-07-19 09:51:20 -0300

    criando menu inicial

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 //Módulo externo
-const inquirer = require('inquirer')
+import inquirer from 'inquirer'
 
 //Módulo interno
-const fs = require('fs')
+import fs from 'fs'
+
+operation()
+
+function operation(){
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    }]
+    ).then((answer) => {
+        const action = answer['action']
+        console.log(action)
+    })
+    .catch((err) => {console.log(err)})
+}
